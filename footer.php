48e6b648224f858528db6044277a2d3103cddf49
pearcey08 <fc_8ljjp@live.ca>
2015-04-06 14:26:27 -0400

    added php pages for footer & nav

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- Footer -->
+        <footer>
+		        <div class="col-lg-12" style="background-color: rgb(29,29,29);">
+			        <div class="container">
+                    	<p>Copyright &copy; Your Website 2014</p>
+			        </div>
+		        </div>
+        </footer>
