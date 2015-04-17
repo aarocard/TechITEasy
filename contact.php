1e00700b07746ac907d823e901666e08cb57192f
JJslaney <slamatta17@gmail.com>
2015-04-17 13:31:27 -0400

    Took off subheadings

--- contact.php
+++ contact.php
@@ -44,9 +44,7 @@
         <!-- Page Heading/Breadcrumbs -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Contact
-                    <small>Subheading</small>
-                </h1>
+                <h1 class="page-header">Contact</h1>
             </div>
         </div>
         <!-- /.row -->
--- services.php
+++ services.php
@@ -44,9 +44,7 @@
         <!-- Page Heading/Breadcrumbs -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Services
-                    <small>Subheading</small>
-                </h1>
+                <h1 class="page-header">Services</h1>
             </div>
         </div>
         <!-- /.row -->
